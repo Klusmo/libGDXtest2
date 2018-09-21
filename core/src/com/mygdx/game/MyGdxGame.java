caadce307087516e4b4dce9035353aa6ca7b60a2
Klusmo <augusto1256@gmail.com>
2018-09-21 09:03:36 -0300

    new hex img

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,15 +10,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	Sprite s;
+	Sprite s[] = new Sprite[10];
+	final float scale = 0.2f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("Hex-Tile.png");
-		s = new Sprite(img);
-		s.setScale(0.1f);
-		s.setPosition(1,1);
+		for (int i = 0; i < 10; i++) {
+			s[i] = new Sprite(img);
+			s[i].setScale(scale);
+			if(i<5)
+				s[i].setPosition(i*s[0].getWidth()*scale, 0);
+			else
+				s[i].setPosition((i-5)*s[0].getWidth()*scale +s[0].getWidth()*scale/2, s[0].getHeight()*scale*3/4);
+		}
 	}
 
 	@Override
@@ -26,7 +32,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(29/255f, 85/255f,173/255f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		s.draw(batch);
+		for (int i = 0; i < 10; i++)
+			s[i].draw(batch);
 		//batch.draw(img, 0, 0);
 		//batch.draw(img, img.getWidth(),0);
 		//batch.draw(img, img.getWidth()/2f,(img.getHeight()*3)/4f);
