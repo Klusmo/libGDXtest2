4c714f6575703390e03f8d1bba7f7913e42d3740
Klusmo <augusto1256@gmail.com>
2018-09-22 23:17:43 -0300

    new hex img

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,40 +6,54 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Sprite s[] = new Sprite[10];
-	final float scale = 0.2f;
-	
+	private SpriteBatch batch;
+	private Texture img;
+	private Sprite s[] = new Sprite[10];
+	private InputController controls ;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("Hex-Tile.png");
-		for (int i = 0; i < 10; i++) {
+		img = new Texture("GHexTile.png" );
+		controls = new InputController();
+		Gdx.input.setInputProcessor(controls);
+
+        for (int i = 0; i < 10; i++) {
 			s[i] = new Sprite(img);
-			s[i].setScale(scale);
-			if(i<5)
-				s[i].setPosition(i*s[0].getWidth()*scale, 0);
-			else
-				s[i].setPosition((i-5)*s[0].getWidth()*scale +s[0].getWidth()*scale/2, s[0].getHeight()*scale*3/4);
-		}
+        }
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(29/255f, 85/255f,173/255f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.graphics.setTitle("FPS: " + Gdx.graphics.getFramesPerSecond());
+
+		for (int i = 0; i < 10; i++) {
+			float scale = controls.getZoom();
+			Vector2 drag = controls.getdPoint();
+            s[i].setScale(scale);
+			//s[i].setPosition((s[i].getX()*scale)-drag.x, (s[i].getY()*scale)-drag.y);
+
+			if(i<5)
+				s[i].setPosition(0-drag.x ,i*s[0].getHeight()*scale + drag.y);
+			else
+				s[i].setPosition((s[0].getWidth()*3/4f)*scale -drag.x,((i-5)*s[0].getHeight()+s[0].getHeight()/2)*scale +drag.y);
+		}
+
+
 		batch.begin();
 		for (int i = 0; i < 10; i++)
 			s[i].draw(batch);
 		//batch.draw(img, 0, 0);
-		//batch.draw(img, img.getWidth(),0);
-		//batch.draw(img, img.getWidth()/2f,(img.getHeight()*3)/4f);
+		//batch.draw(img, img.getWidth()*3/2f,0);
+		//batch.draw(img, img.getWidth()3/2f,(img.getHeight()*3)/4f);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
